982b268d5aecbb3e69142d982439731b852a4352
Luis Otávio <luis@MacBook-Air-de-Luis.local>
2025-09-22 20:48:13 -0300

    keypad update

--- Scripts/KeyPad.gd
+++ Scripts/KeyPad.gd
@@ -1,1 +1,10 @@
 extends StaticBody3D
+
+@onready var label: Label = $SubViewport/Label
+@export var password : String
+@export var door : StaticBody3D
+var current : String
+
+func addNumber(num):
+	current += num
+	label.text = current
--- Scripts/KeyPadButton.gd
+++ Scripts/KeyPadButton.gd
@@ -1,1 +1,14 @@
 extends StaticBody3D
+
+##This is the name that will appear on the HUD when looking at it
+@export var objectName := "Holder"
+
+@export var number : String
+@onready var label: Label = $SubViewport/Label
+@onready var key_pad: StaticBody3D = $"../.."
+
+func _ready() -> void:
+	label.text = number
+
+func _interact():
+	key_pad.addNumber(number)
